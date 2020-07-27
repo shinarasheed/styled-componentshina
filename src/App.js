3780064917318abb74f568746824b60840bbe100
shina <rasheedshinaopeyemi@gmail.com>
2020-07-27 22:45:38 +0100

    added global styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import Home from "./pages/Home";
+import "./components/globals/GlobalStyles";
 
 function App() {
-  return <div>hello</div>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
---
+++ src/styles.js
@@ -0,0 +1,12 @@
+export const setColor = {
+  primaryColor: "#af9a7d",
+  mainWhite: "#fff",
+  mainBlack: "#222",
+  mainGrey: "#ececec",
+  lightGrey: "#f7f7f7",
+};
+
+export const setFont = {
+  mainFont: "font-family: 'Open Sans', sans-serif;",
+  slanted: " font-family: 'Courgette', cursive;",
+};
