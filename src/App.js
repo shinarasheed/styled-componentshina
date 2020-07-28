b836c1395a0b22e4cd5f98f5aee99054fcc6a162
shina <rasheedshinaopeyemi@gmail.com>
2020-07-28 08:10:11 +0100

    removed globalstyles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
-import GlobalStyles from "./components/globals/GlobalStyles";
 
 function App() {
   return (
     <>
-      <GlobalStyles />
       <Home />
     </>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../components/home/Header";
+// import GlobalStyles from "../components/globals/GlobalStyles";
 
 const Home = () => {
   return (
